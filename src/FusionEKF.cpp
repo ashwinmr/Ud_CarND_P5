51de5b47908f0e0e9777ae0f37ef4401f46a2a42
Ashwin <ashwin.rao@getcruise.com>
2018-12-30 09:01:48 -0800

    Initialize FusionEKF

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,8 +36,12 @@ FusionEKF::FusionEKF() {
    * TODO: Finish initializing the FusionEKF.
    * TODO: Set the process and measurement noises
    */
+  H_laser_ << 1,0,0,0,
+              0,1,0,0;
 
-
+  Hj_ << 0,0,0,0,
+         0,0,0,0,
+         0,0,0,0;
 }
 
 /**
@@ -62,8 +66,9 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     ekf_.x_ << 1, 1, 1, 1;
 
     if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
-      // TODO: Convert radar from polar to cartesian coordinates 
+      // TODO: Convert radar from polar to cartesian coordinates
       //         and initialize state.
+
 
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
